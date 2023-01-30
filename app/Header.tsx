f2416950410e004b6947a4646961db67d162075e
Muhammad Konain <konainkhan0101@gmail.com>
2023-01-30 14:48:09 -0800

    add pricingui page

---
+++ app/Header.tsx
@@ -0,0 +1,17 @@
+import { Box, Heading, Text} from "@chakra-ui/react";
+import React from 'react'
+
+export default function Header() {
+  return(
+  <Box textAlign={"center"}bg="#6846C1" pt="150px" pb="200px" color="white" >
+
+    <Heading pb="10px">Simple Pricing For Your Business</Heading>
+    <Text pt="10px">Plans that are Carefully Crafted to Suit Your Business. </Text>
+  </Box>
+
+);
+
+
+
+}
+ 
